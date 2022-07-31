1855896018212f8619a4d3a65c91d73e42debcb5
Faisalm <feisalmohammadi@gmail.com>
2022-07-31 23:24:05 +0200

    added app configurations

---
+++ lib/common/configs/configs.dart
@@ -0,0 +1,11 @@
+export 'app_dimensions.dart';
+export 'app_typography.dart';
+export '../extension/app_typography_ext.dart';
+export '../themes/app_theme.dart';
+export 'ui_props.dart';
+export 'space.dart';
+export 'space_ext.dart';
+export 'ui.dart';
+
+export '../themes/core_theme.dart' show fontFamily;
+export 'date_ext.dart';
